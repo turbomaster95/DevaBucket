4801c62223e8d4c9d357fdd6a595282e4eca740d
Deva Midhun <dev@deva.is-cool.dev>
2024-01-01 15:19:57 +0530

    Update auto-pr.ps1

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "<username>/<bucketname>:main"
+    [String]$upstream = "turbomaster95/DevaBucket:main"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
